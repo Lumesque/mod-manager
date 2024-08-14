15d6c4103f691bc288cfa14afa05b664bad8fdbd
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-08-14 18:22:12 -0400

    Updating for ruff tooling in development

--- flake.nix
+++ flake.nix
@@ -19,6 +19,7 @@
           pp.click
           pp.twine
         ]))
+        pkgs.ruff
       ];
     };
     packages.${system}.default = pkgs.symlinkJoin {
@@ -30,6 +31,7 @@
           pp.click
           pp.twine
         ]))
+        pkgs.ruff
       ];
     };
   };
